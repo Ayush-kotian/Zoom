23747328373780de0d3b75ddfa00368baeaabc92
Ayush-Kotian <ssanilayush@gmail.com>
2026-05-09 15:30:12 +0530

    landingPage done

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -5,8 +5,15 @@ let messages = {};
 let timeOnline = {};
 
 export const connectToSocket = (server) => {
-    const io = new Server(server);
-
+    // const io = new Server(server);
+      const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            allowedHeaders: ["*"],
+            credentials: true
+        }
+    });
     io.on("connection", (socket) => {
         console.log("SOMETHING CONNECTED");
 
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,2 +1,32 @@
 import React from "react";
-export default landing
+import { Link } from "react-router-dom";
+export default function Landing() {
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>Apna Video Call</h2>
+        </div>
+        <div className="navList">
+          <p>Join</p>
+          <p>Register</p>
+          <div role="button">
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+      <div className="landingMainContainer">
+        <div>
+          <h1><span style={{color: "#FF9839"}}>Connect </span>with your <br/> Loved Ones</h1>
+          <p>Cover a distance by apna video call</p>
+          <div role="button">
+            <Link to="/auth">Get Started</Link>
+          </div>
+        </div>
+        <div>
+          <img src="/mobile.png"/>
+        </div>
+      </div>
+    </div>
+  );
+}
